f0fb65c2cd99025922e7a92ddc9323bf8a78dff8
Alejandro Berrio <alejo.berrio@duke.edu>
2024-11-07 10:50:04 -0500

    Create sessionInfo.R

---
+++ sessionInfo.R
@@ -0,0 +1,57 @@
+R version 4.2.2 (2022-10-31)
+Platform: x86_64-conda-linux-gnu (64-bit)
+Running under: CentOS Linux 7 (Core)
+
+Matrix products: default
+BLAS/LAPACK: /gpfs/fs1/data/wraycompute/alejo/conda/Seurat4_3/lib/libopenblasp-r0.3.21.so
+
+locale:
+ [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C              
+ [3] LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8    
+ [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8   
+ [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                 
+ [9] LC_ADDRESS=C               LC_TELEPHONE=C            
+[11] LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+ [1] lubridate_1.9.2    forcats_1.0.0      stringr_1.5.0      dplyr_1.1.0       
+ [5] purrr_1.0.1        readr_2.1.4        tidyr_1.3.0        tibble_3.1.8      
+ [9] ggplot2_3.4.1      tidyverse_2.0.0    SeuratObject_4.1.3 Seurat_4.3.0      
+
+loaded via a namespace (and not attached):
+ [1] nlme_3.1-162           matrixStats_0.63.0     spatstat.sparse_3.0-0 
+ [4] RcppAnnoy_0.0.20       RColorBrewer_1.1-3     httr_1.4.5            
+ [7] sctransform_0.3.5      tools_4.2.2            utf8_1.2.3            
+[10] R6_2.5.1               irlba_2.3.5.1          KernSmooth_2.23-20    
+[13] uwot_0.1.14            lazyeval_0.2.2         colorspace_2.1-0      
+[16] withr_2.5.0            sp_1.6-0               tidyselect_1.2.0      
+[19] gridExtra_2.3          compiler_4.2.2         progressr_0.13.0      
+[22] cli_3.6.0              spatstat.explore_3.0-6 plotly_4.10.1         
+[25] scales_1.2.1           lmtest_0.9-40          spatstat.data_3.0-0   
+[28] ggridges_0.5.4         pbapply_1.7-0          goftest_1.2-3         
+[31] digest_0.6.31          spatstat.utils_3.0-1   pkgconfig_2.0.3       
+[34] htmltools_0.5.4        parallelly_1.34.0      fastmap_1.1.1         
+[37] htmlwidgets_1.6.1      rlang_1.0.6            shiny_1.7.4           
+[40] generics_0.1.3         zoo_1.8-11             jsonlite_1.8.4        
+[43] spatstat.random_3.1-3  ica_1.0-3              magrittr_2.0.3        
+[46] patchwork_1.1.2        Matrix_1.5-3           Rcpp_1.0.10           
+[49] munsell_0.5.0          fansi_1.0.4            abind_1.4-5           
+[52] reticulate_1.28        lifecycle_1.0.3        stringi_1.7.12        
+[55] MASS_7.3-58.2          Rtsne_0.16             plyr_1.8.8            
+[58] grid_4.2.2             parallel_4.2.2         listenv_0.9.0         
+[61] promises_1.2.0.1       ggrepel_0.9.3          deldir_1.0-6          
+[64] miniUI_0.1.1.1         lattice_0.20-45        cowplot_1.1.1         
+[67] splines_4.2.2          tensor_1.5             hms_1.1.2             
+[70] pillar_1.8.1           igraph_1.4.1           spatstat.geom_3.0-6   
+[73] future.apply_1.10.0    reshape2_1.4.4         codetools_0.2-19      
+[76] leiden_0.4.3           glue_1.6.2             data.table_1.14.8     
+[79] tzdb_0.3.0             png_0.1-8              vctrs_0.5.2           
+[82] httpuv_1.6.9           polyclip_1.10-4        gtable_0.3.1          
+[85] RANN_2.6.1             scattermore_0.8        future_1.31.0         
+[88] mime_0.12              xtable_1.8-4           later_1.3.0           
+[91] survival_3.5-3         viridisLite_0.4.1      cluster_2.1.4         
+[94] timechange_0.2.0       globals_0.16.2         fitdistrplus_1.1-8    
+[97] ellipsis_0.3.2         ROCR_1.0-11
